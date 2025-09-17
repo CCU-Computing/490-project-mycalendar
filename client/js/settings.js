8f4114dce23dd21dd968b85ce407c0f9dfa9e34c
BlizzardRAZE <trmiller2003@gmail.com>
2025-09-16 22:00:16 -0400

    Copyed over the dashboard/settings.js and settings.html from color-code-testing branch to see the overall layout.Not only that, I used it to compare the ICS file it made with our application. Overall, users are able to download a ICS file to their desired calender, plus color coding as been added to each event, so it should generate a color for it.

--- client/js/settings.js
+++ client/js/settings.js
@@ -13,105 +13,176 @@
         userChip.textContent = "Hi, " + userName;
     }
 
+
+    // COLOR PICKER
     // API Calls
     // Get Courses and Database(JSON)
-    const [{ courses }, { prefs }] = await Promise.all([
-        fetch('/api/courses').then(r => r.json()),
-        fetch('/api/prefs').then(r => r.json())
-    ]);
-    const courseColors = prefs?.calendar?.courseColors || {};
-    
-    let cardDeck = document.getElementById("cardDeck");
-    
-    // Go through and populate course settings
-    courses.forEach(course => {
-        // COURSE CARD LAYOUT
-        let courseCard = document.createElement("div");
-        courseCard.id = "courseCard";
-        courseCard.dataset.courseId = course.id;
-        courseCard.className = "flex justify-between items-center rounded-xl border border-b border-slate-300 bg-slate-50 p-4 text-sm";
+    try {
+        const [{ courses }, { prefs }] = await Promise.all([
+            fetch('/api/courses').then(r => r.json()),
+            fetch('/api/prefs').then(r => r.json())
+        ]);
 
-        let courseName = document.createElement("span");
-        courseName.id = "courseName";
-        courseName.innerHTML = course.name;
-        courseName.className = "text-sm text-slate-500 mt-1";
+        const courseColors = prefs?.calendar?.courseColors || {};
+        
+        let cardDeck = document.getElementById("cardDeck");
+        
+        // Go through and populate course settings
+        // NOTE: Talk about this: How we could make it load faster? Session, etc..
+        courses.forEach(course => {
+            // COURSE CARD LAYOUT
+            let courseCard = document.createElement("div");
+            courseCard.id = "courseCard";
+            courseCard.dataset.courseId = course.id;
+            courseCard.className = `flex justify-between items-center rounded-xl border bg-slate-50 p-4 text-sm`;
 
-        let colorInput = document.createElement("input");
-        colorInput.id = "colorInput";
-        colorInput.type = "color";
-        colorInput.className = "w-10 h-8 rounded-full border-2 border-gray-300 appearance-none cursor-pointer color-picker";
+            let courseName = document.createElement("span");
+            courseName.id = "courseName";
+            courseName.innerHTML = course.name;
+            courseName.className = "pr-2 text-sm text-slate-500 mt-1";
 
-        // Update local state (visual only)
-        colorInput.addEventListener("change", (e) => {
-            colorInput.value = e.target.value;
-        });
+            let colorInput = document.createElement("input");
+            colorInput.id = "colorInput";
+            colorInput.type = "color";
+            colorInput.className = "w-10 aspect-square rounded-full border-2 border-gray-300 appearance-none cursor-pointer color-picker shrink-0";
 
-        // Save when user is done picking
-        colorInput.addEventListener("blur", async (e) => {
-            const newColor = e.target.value;
-
-            await fetch('/api/prefs/courseColor', {
-                method: 'PUT',
-                headers: { 'Content-Type': 'application/json' },
-                body: JSON.stringify({ courseId: course.id, color: newColor })
+            // Update local state (visual only)
+            colorInput.addEventListener("change", (e) => {
+                colorInput.value = e.target.value;
             });
 
-            console.log(`Saved ${newColor} for course ${course.id}`);
+            colorInput.addEventListener("input", (e) => {
+                courseCard.style.borderColor = e.target.value;
+            });
+
+            // Save when user is done picking
+            colorInput.addEventListener("blur", async (e) => {
+                const newColor = e.target.value;
+
+                await fetch('/api/prefs/courseColor', {
+                    method: 'PUT',
+                    headers: { 'Content-Type': 'application/json' },
+                    body: JSON.stringify({ courseId: course.id, color: newColor })
+                });
+
+            });
+            
+            // Append 
+            courseCard.appendChild(courseName);
+            courseCard.appendChild(colorInput);
+
+            cardDeck.appendChild(courseCard);
+
+            colorInput.value = courseColors[String(course.id)] || '#4F46E5';
+            courseCard.style.borderColor = colorInput.value;
         });
-        
-        // Append 
-        courseCard.appendChild(courseName);
-        courseCard.appendChild(colorInput);
-
-        cardDeck.appendChild(courseCard);
-
-        colorInput.value = courseColors[String(course.id)] || '#4F46E5';
-    });
-
+    } catch (error) {
+        console.error(error);
+    }
 
     // EXPORT FUNCTION
-    let icsButton = document.getElementById("exportICS");
-    // ICS
+    try {
+        // API Calls
+        const [{ courses }, { prefs }, { events }] = await Promise.all([
+            fetch('/api/courses').then(r => r.json()),
+            fetch('/api/prefs').then(r => r.json()),
+            fetch('/api/calendar').then(r => r.json())
+        ]);
 
-    icsButton.addEventListener("click", () => {
-        window.alert("Downloading...");
+        // Get Colors from Database (JSON)
+        const courseColors = prefs?.calendar?.courseColors || {};
 
-        // OUTLINE FOR EACH EVENT
-        const icsCONTENT = `
-            BEGIN:VCALENDAR
-            VERSION:2.0
-            CALSCALE:GREGORIAN
-            METHOD:PUBLISH
+        // ICS
+        let icsButton = document.getElementById("exportICS");
 
-            BEGIN:VEVENT
-            UID:${Date.now()}@example.com
-            DTSTAMP:${formatDate(new Date())}
-            SUMMARY:My Custom Event
-            DESCRIPTION:This is an example event
-            DTSTART:20250915T090000Z
-            DTEND:20250915T100000Z
-            LOCATION:Online
-            END:VEVENT
+        // Declare Header, Foot, and Body
+        // I know it looks weird, but it works :)
+        let icsHeader = `BEGIN:VCALENDAR
+VERSION:2.0
+PRODID:-//MyCalendar//EN
+CALSCALE:GREGORIAN
+`;
+        let icsFooter = `END:VCALENDAR`;
+    
+    
+        // Store each icsEvent into icsContainer
+        let icsContainer = ""; 
+        
 
-            END:VCALENDAR`;
-
-        // helper to format dates into iCalendar UTC format
-        function formatDate(date) {
+        // Format date to ICS (UTC)
+        function toICSDate(date) {
         return date.toISOString().replace(/[-:]/g, "").split(".")[0] + "Z";
         }
 
-        // Place download file into button
-        const blob = new Blob([icsCONTENT], {type:"text/calendar;charset=utf-8"})
-        const url = URL.createObjectURL(blob);
+        // Go thorugh each Course Event
+        // Find the Event that matches the Course Id
+        // Set a event for it 
+        courses.forEach(course => {
+            // Get Course Id and Name
+            let courseId = course.id;
+            let courseName = course.name;
+            events.forEach(event => {
+                // Get assignment info
+                let assignmentCourseId = event.courseId;
+                let assignmentId = event.id;
+                let assignmentType = event.type;
+                let assignmentName = event.title;
+                let assignmentDue = new Date(event.dueAt * 1000);
+                let assignmentLocation = "Moodle"
+                let courseColor = courseColors[String(course.id)] || '#4F46E5';
+                
+                
+                // Check to see if courseId matches with assignmentCourseId
+                if (courseId ===  assignmentCourseId) {
+                    // Set convert UNIX to UTC
+                    let dtpStamp = toICSDate(new Date());
+                    let dtpStart = toICSDate(assignmentDue);
+                    let dtpEnd = toICSDate(new Date(assignmentDue.getTime() + 30*60*1000));
 
-        const link = document.createElement("a");
-        link.href = url;
-        link.download = "sample.ics";
-        document.body.appendChild(link);
-        link.click();
-        document.body.removeChild(link);
+                    // Each event will follow this structure. (No spaces/tabs)
+                    // I know it looks weird, but it works :)
+                    let icsBody = `BEGIN:VEVENT
+UID:${assignmentId}@mycalendar
+DTSTAMP:${dtpStamp}
+DTSTART:${dtpStart}
+DTEND:${dtpEnd}
+SUMMARY:${assignmentName}
+DESCRIPTION:This is a ${assignmentType} for ${courseName}.
+LOCATION:${assignmentLocation}
+COLOR:${courseColor}
+X-APPLE-CALENDAR-COLOR:${courseColor}
+END:VEVENT
+`;
 
-    })
+                    // Append to container
+                    icsContainer += icsBody;
+                }
+            });
+        });
+
+        // Assemble file
+        let icsContent = icsHeader +  icsContainer + icsFooter;   
+        
+        console.log(JSON.stringify(icsContent));
+
+        icsButton.addEventListener("click", () => {
+            // Place download file into button
+            const blob = new Blob([icsContent], {type:"text/calendar;charset=utf-8"})
+            const url = URL.createObjectURL(blob);
+
+            const link = document.createElement("a");
+            link.href = url;
+            link.download = "MyCalendar.ics";
+            document.body.appendChild(link);
+            link.click();
+            document.body.removeChild(link);
+
+        })
+
+    } catch (error) {
+        console.error(error);
+    }
+    
 })();
 
 
